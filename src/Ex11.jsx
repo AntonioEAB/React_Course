d2725c73e0d569a375e7d5d872dd6fc30c73d648
AntonioEAB <antonioaviles908@gmail.com>
2024-04-30 12:51:07 -0600

    New changes

--- src/Ex11.jsx
+++ src/Ex11.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './Components/Note'
+import noteService from './Services/notes';
 
 const Ex11 = () => {
 
@@ -8,6 +8,27 @@ const Ex11 = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] =useState('Algo acaba de pasar')
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+      .update(changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
 
   //Actualiza cada vez que cambia la entrada de una nueva cadena.
   const handleNoteChange = (event) => {
@@ -23,36 +44,62 @@ const Ex11 = () => {
 
   //Permite tener un efecto al renderizar estara a la par con las actualizaciones de nuetra página.
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
+    noteService
+    .getAll()
+    .then(initialNotes => {
+      setNotes(initialNotes)
+    })
+  }
+  , [])
   console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
-    
+
     event.preventDefault()
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
     }
 
-    axios
-    .post('http://localhost:3001/notes', noteObject)
-    .then(response => {
-      setNotes(notes.concat(response.data))
-      setNewNote('')
-      console.log(response)
-    })
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
 
+
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+  
+    return (
+      <div className="error">
+        {message}
+      </div>
+    )
+  }
+
+  const Footer = () => {
+    const footerStyle = {
+      color: 'green',
+      fontStyle: 'italic',
+      fontSize: 16
+    }
+    return (
+      <div style={footerStyle}>
+        <br />
+        <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+      </div>
+    )
+  }
+  
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
@@ -60,13 +107,14 @@ const Ex11 = () => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note}  toggleImportance={() => toggleImportanceOf(note.id)} />
         )}
       </ul>
       <form onSubmit={addNote}>
       <input value={newNote}   onChange={handleNoteChange}/>
         <button type="submit">save</button>
-      </form>   
+      </form>  
+      <Footer /> 
     </div>
   )
 }
--- src/Ex2.jsx
+++ src/Ex2.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import Ex11 from './Ex11'
 import { StrictMode } from 'react'
+import './index.css'
 
 
   ReactDOM.createRoot(document.getElementById('root')).render(<StrictMode><Ex11 /></StrictMode>)
